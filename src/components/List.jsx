ce50b9e71c4b195d0e50a625c7f8fbf68a748043
Rúben Freitas <joseruben98@hotmail.com>
2025-05-06 10:37:54 +0100

    small channel list update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,60 +1,85 @@
 import React, { useState } from "react";
-import { Link } from "react-router";
-import Channel from "./Channel";
 
 const daddyliveurl = "https://thedaddy.to/embed/";
+const topembedurl = "https://topembed.pw/channel/";
 
 const channels = [
   {
     name: "SportTV 1",
-    urls: [daddyliveurl + "stream-49.php"],
+    urls: [daddyliveurl + "stream-49.php", topembedurl + "SportTV1[Portugal]"],
   },
   {
     name: "SportTV 2",
-    urls: [daddyliveurl + "stream-74.php"],
+    urls: [daddyliveurl + "stream-74.php", topembedurl + "SportTV2[Portugal]"],
   },
   {
     name: "SportTV 3",
-    urls: [daddyliveurl + "stream-454.php"],
+    urls: [daddyliveurl + "stream-454.php", topembedurl + "SportTV3[Portugal]"],
   },
   {
     name: "SportTV 4",
-    urls: [daddyliveurl + "stream-289.php"],
+    urls: [daddyliveurl + "stream-289.php", topembedurl + "SportTV4[Portugal]"],
   },
   {
     name: "SportTV 5",
-    urls: [daddyliveurl + "stream-290.php"],
+    urls: [daddyliveurl + "stream-290.php", topembedurl + "SportTV5[Portugal]"],
+  },
+
+  {
+    name: "SportTV 6",
+    urls: [topembedurl + "SportTV6[Portugal]"],
   },
   {
     name: "ElevenSports1",
-    urls: [daddyliveurl + "stream-291.php"],
+    urls: [
+      daddyliveurl + "stream-291.php",
+      topembedurl + "ElevenSports1[Portugal]",
+    ],
   },
   {
     name: "ElevenSports2",
-    urls: [daddyliveurl + "stream-456.php"],
+    urls: [
+      daddyliveurl + "stream-456.php",
+      topembedurl + "ElevenSports2[Portugal]",
+    ],
   },
   {
     name: "ElevenSports3",
-    urls: [daddyliveurl + "stream-457.php"],
+    urls: [
+      daddyliveurl + "stream-457.php",
+      topembedurl + "ElevenSports3[Portugal]",
+    ],
   },
   {
     name: "ElevenSports4",
-    urls: [daddyliveurl + "stream-458.php"],
+    urls: [
+      daddyliveurl + "stream-458.php",
+      topembedurl + "ElevenSports4[Portugal]",
+    ],
   },
   {
     name: "ElevenSports5",
     urls: [
-      "https://topembed.pw/channel/ElevenSports5[Portugal]",
+      topembedurl + "ElevenSports5[Portugal]",
       daddyliveurl + "stream-459.php",
     ],
+  },
+  {
+    name: "ElevenSports6",
+    urls: [topembedurl + "ElevenSports6[Portugal]"],
   },
   {
     name: "Sky Sports F1",
     urls: [
       daddyliveurl + "stream-60.php",
       "https://letscast.pro/bill.php?stream=91LHlS9qZPK5",
-      "https://topembed.pw/channel/SkySportsF1[UK]",
+      topembedurl + "SkySportsF1[UK]",
     ],
+  },
+
+  {
+    name: "DAZN1",
+    urls: [topembedurl + "DAZN1[Portugal]"],
   },
 ];
 
@@ -67,7 +92,7 @@ function List() {
     setUrlIndex(0);
   };
   return (
-    <div>
+    <div className="page">
       <ul>
         {channels.map((channel) => (
           <li onClick={() => handleChannelClick(channel)} key={channel.name}>
@@ -77,7 +102,7 @@ function List() {
       </ul>
 
       {currentChannel && (
-        <div>
+        <div className="video-container">
           <div>
             {currentChannel.urls.map((url, index) => (
               <button
@@ -92,7 +117,6 @@ function List() {
           </div>
           <iframe
             className="video responsive"
-            style={{ margin: "0px", width: "100%", height: "60vh" }}
             src={currentChannel.urls[urlIndex]}
             name="iframe_a"
             allowFullScreen="yes"
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import List from "./components/List";
-import Channel from "./components/Channel";
+import "./index.css";
+import List from "./components/List.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<List />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <List />
+  </StrictMode>
 );
